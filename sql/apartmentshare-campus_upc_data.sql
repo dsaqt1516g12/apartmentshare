13a01da48fc69ad264a886cd6b6e9f18c1333cf3
Jordi Lopez <jordi.lopez@estudiant.upc.edu>
2016-01-12 19:30:18 +0100

    added backup table campus_upc

---
+++ sql/apartmentshare-campus_upc_data.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.6.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: apartmentsharedb
+-- ------------------------------------------------------
+-- Server version	5.6.26-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `campus_upc`
+--
+
+DROP TABLE IF EXISTS `campus_upc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `campus_upc` (
+  `id` binary(16) NOT NULL,
+  `campusname` varchar(20) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `longitud` float(17,14) NOT NULL,
+  `latitud` float(17,14) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `campus_upc`
+--
+
+LOCK TABLES `campus_upc` WRITE;
+/*!40000 ALTER TABLE `campus_upc` DISABLE KEYS */;
+INSERT INTO `campus_upc` VALUES ('1¢¢πXÂÅ8\0#•lå','EETAC2','Av. del Canal Olimpic, 7 08860 Castelldefels Barcelona, Espa√±a',41.27210235595703,1.99207901954651),('yòI°–Â´ı\0#•lå','EETAC','Av. del Canal Olimpic, 7 08860 Castelldefels Barcelona, Espa√±a',41.27210235595703,1.99207901954651),('¥àÏ.πWÂÅ8\0#•lå','EETAC2','Av. del Canal Olimpic, 7 08860 Castelldefels Barcelona, Espa√±a',41.27210235595703,1.99207901954651);
+/*!40000 ALTER TABLE `campus_upc` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-01-12 19:29:45
