7b73db62c04bbfac2db62792d89d76c39d4c09d6
Jordi Lopez <jordi.lopez@estudiant.upc.edu>
2016-01-17 10:59:36 +0100

    crearpiso.htmp fix v3

--- www/js/crearpiso.js
+++ www/js/crearpiso.js
@@ -125,7 +125,7 @@ $(function(){
 $('#myaddress').donetyping(function(){
   $(document).ready(function(){
   $("address").each(function(){                         
-    var embed ="<iframe width='425' height='350' frameborder='0' scrolling='no'  marginheight='0' marginwidth='0'   src='https://maps.google.com/maps?&amp;q="+ encodeURIComponent( $('#myaddress').val() ) +"&amp;output=embed'></iframe>";
+    var embed ="<iframe width='450' height='350' frameborder='0' scrolling='no'  marginheight='0' marginwidth='0'   src='https://maps.google.com/maps?&amp;q="+ encodeURIComponent( $('#myaddress').val() ) +"&amp;output=embed'></iframe>";
                                 $(this).html(embed);
                              
   });
