c78b437235e7eaaeed510c16611141373d4fe4c1
Jordi Lopez <jordi.lopez@estudiant.upc.edu>
2016-01-19 18:51:33 +0100

    add doc and fix CORS

--- service/src/main/java/edu/upc/eetac/dsa/apartmentshare/cors/CORSResponseFilter.java
+++ service/src/main/java/edu/upc/eetac/dsa/apartmentshare/cors/CORSResponseFilter.java
@@ -10,16 +10,16 @@ import java.io.IOException;
  */
 public class CORSResponseFilter implements ContainerResponseFilter {
     @Override
-    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
-        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        String reqHead = containerResponseContext.getHeaderString("Access-Control-Request-Headers");
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        String reqHead = requestContext.getHeaderString("Access-Control-Request-Headers");
 
         if (null != reqHead && !reqHead.equals("")) {
-            containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", reqHead);
+            responseContext.getHeaders().add("Access-Control-Allow-Headers", reqHead);
         }
 
-        containerResponseContext.getHeaders().add("Access-Control-Expose-Headers", "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Location");
+        responseContext.getHeaders().add("Access-Control-Expose-Headers", "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Location");
     }
     }
 
